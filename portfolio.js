a1a11c8b69cddb62fecaa027f9348ef629776545
Simone Juul Andersen <simoneandersen@Simones-MacBook-Pro-3.local>
2018-01-05 13:34:34 +0100

    ..

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+$(window).on("load", start);
+
+function start() {
+    console.log("start");
+    $("#ikon_container").addClass("ikon_container");
+    $("#ikon_sprite").addClass("ikon_skifter");
+}
